059c3da145ca7a4501f7d7e6595111866caf6599
Frank Agbenyo Fugah <fugah@trestleacademyghana.org>
2024-06-08 12:44:49 +0000

    configuration script

--- src/config.py
+++ src/config.py
@@ -7,12 +7,14 @@ def read_config():
     aws_config = {
         'aws_access_key_id': config.get('aws', 'AWS_ACCESS_KEY'),
         'aws_secret_access_key': config.get('aws', 'AWS_SECRET_KEY'),
-        'region_name': config.get('aws', 'AWS_REGION')
+        'region_name': config.get('aws', 'AWS_REGION'),
+        's3_bucket': config.get('aws', 'S3_BUCKET_NAME')
     }
     
     athena_config = {
         'database': config.get('athena', 'SCHEMA_NAME'),
-        's3_output_location': config.get('athena', 'S3_STAGING_DIR')
+        's3_output_location': config.get('athena', 'S3_OUTPUT_DIRECTORY'),
+        's3_staging_dir': config.get('athena', 'S3_STAGING_DIR')        
     }
     
     return aws_config, athena_config
